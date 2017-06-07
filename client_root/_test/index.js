e91c55741fbaba9a96f37524e84c3c1f99909ea1
Ramesh Kumar <codeofnode@gmail.com>
2017-06-07 12:10:00 +0530

    no tests

--- client_root/_test/index.js
+++
@@ -1,7 +0,0 @@
-const _ = require(process.cwd()+'/_internal/mockObject').client(),
-  testApi = require(process.cwd()+'/_internal/testApi');
-
-var testData = [
-];
-
-testData.forEach(testApi.bind(null,exports,_));
